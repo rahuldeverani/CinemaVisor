6ce37a6e3a4da37b0b425b2b7f85a51057166031
unknown <rahuldeverani@gmail.com>
2018-08-22 12:25:52 +0530

    Ui changes

--- all/app.js
+++ all/app.js
@@ -2,17 +2,35 @@
 var Request = require("request");
 var express=require('express');
 //const mongoose    =require('mongoose')
-//const bodyparser  =require('body-parser');
+const bodyparser  =require('body-parser');
 //mongoose.connect("mongodb://localhost:27017/moviesite", { useNewUrlParser: true });
 
 
 
 
 var app=express();
-//app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.urlencoded({extended:false}))
 app.use(express.static(__dirname+"/public"));
-app.get('/',function(req,res){
-    Request.get("https://api.themoviedb.org/4/list/1?api_key=2b6f6b0f9f52bbfa3376c020de4832e3", (error, response, body) => {
+app.get('/tvshows',function(req,res){
+
+    Request.get("https://api.themoviedb.org/3/tv/popular?api_key=2b6f6b0f9f52bbfa3376c020de4832e3"+"&page="+req.params.id, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+    
+        var tvshows=JSON.parse(body);
+       console.log(tvshows.results[0].title);
+       res.render('index.ejs',{movies:tvshows})
+
+
+})
+
+
+
+
+});
+app.get('/:id',function(req,res){
+    Request.get("https://api.themoviedb.org/4/list/1?api_key=2b6f6b0f9f52bbfa3376c020de4832e3"+"&page="+req.params.id, (error, response, body) => {
         if(error) {
             return console.dir(error);
         }
@@ -22,11 +40,9 @@ app.get('/',function(req,res){
        res.render('index.ejs',{movies:e})
     
     });
+}
+)
 
-
-
-  
-})
 
 
 app.listen(3030);
