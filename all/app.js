a614d8d89748030e77f2e54f687d7b67e46607f5
unknown <rahuldeverani@gmail.com>
2018-09-08 17:03:52 +0530

    Added movies to discover page

--- all/app.js
+++ all/app.js
@@ -352,17 +352,39 @@ console.log(all);
 })
 
 
-app.get('/discover-movies',function(re1,res){
+app.get('/discover-movies',function(req,res){
+    var myhead="Popular Movies"
+    Request.get("https://api.themoviedb.org/3/movie/popular?api_key=2b6f6b0f9f52bbfa3376c020de4832e3&language=en-US"+"&page="+req.params.id, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+    
+        var popmov=JSON.parse(body);
+      // console.log(tvshows.results[0].title);
+       res.render('discover.ejs',{movies:popmov,myhead:myhead})
 
-res.render('discover.ejs');
+
+})
+
 
 })
 
 
 
-app.get('/discover-tvshows',function(re1,res){
+app.get('/discover-tvshows',function(req,res){
 
-    res.render('discover.ejs');
+    var myhead="Popular Movies"
+    Request.get("https://api.themoviedb.org/3/movie/popular?api_key=2b6f6b0f9f52bbfa3376c020de4832e3&language=en-US"+"&page="+req.params.id, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+    
+        var popmov=JSON.parse(body);
+      // console.log(tvshows.results[0].title);
+       res.render('discover.ejs',{movies:popmov,myhead:myhead})
+
+
+})
     
     })
 
